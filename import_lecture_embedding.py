55db437e5a70f28b33648eee02519026d581c5b6
Soohyun Park <ardeness@gmail.com>
2024-10-23 09:41:39 +0900

    Add efs case

--- import_lecture_embedding.py
+++ import_lecture_embedding.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.configuration import conf
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
+from kubernetes.client import models as k8s
 
 namespace = conf.get('kubernetes', 'NAMESPACE') # This will detect the default namespace locally and read the
 
@@ -20,13 +21,20 @@ def create_dag(schedule, default_args):
     dag = DAG(dag_id, tags=[project], schedule_interval=schedule, default_args=default_args, is_paused_upon_creation=False)
 
     secret_env = Secret("env",None,"lecture-rag")
-
+    volume = k8s.V1Volume(
+        name="efs-claim",
+        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="efs-claim"),
+    )
+    volume_mount = k8s.V1VolumeMount(
+        name="efs-claim",
+        mount_p9ath="/opt/data"
+    )
     with dag:
         import_lecture_embedding =  KubernetesPodOperator(
             namespace=namespace,
             image = "024848470331.dkr.ecr.ap-northeast-2.amazonaws.com/hycu/lecture-rag:latest",
             image_pull_policy='Always',
-            cmds = ["python", "embedding_extract.py", "13.pdf", "dag"],
+            cmds = ["python", "embedding_extract.py", "/opt/data/13.pdf", "test"],
             name="task-"+project+"-import-lecture-embedding",
             task_id="task-"+project+"-import-lecture-embedding",
             in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
@@ -35,7 +43,8 @@ def create_dag(schedule, default_args):
             #resources=compute_resources,
             is_delete_operator_pod=True,
             get_logs=True,
-            secrets = [secret_env]
+            secrets = [secret_env],
+            volume_mounts=volume_mount
         )
         import_lecture_embedding
 
--- srt_correction.py
+++ srt_correction.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.configuration import conf
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
+from kubernetes.client import models as k8s
 
 namespace = conf.get('kubernetes', 'NAMESPACE') # This will detect the default namespace locally and read the
 
@@ -20,13 +21,20 @@ def create_dag(schedule, default_args):
     dag = DAG(dag_id, tags=[project], schedule_interval=schedule, default_args=default_args, is_paused_upon_creation=False)
 
     secret_env = Secret("env",None,"lecture-rag")
-
+    volume = k8s.V1Volume(
+        name="efs-claim",
+        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="efs-claim"),
+    )
+    volume_mount = k8s.V1VolumeMount(
+        name="efs-claim",
+        mount_p9ath="/opt/data"
+    )
     with dag:
         srt_correction =  KubernetesPodOperator(
             namespace=namespace,
             image = "024848470331.dkr.ecr.ap-northeast-2.amazonaws.com/hycu/lecture-rag:latest",
             image_pull_policy='Always',
-            cmds = ["python", "correction.py", "13.srt", "dag"],
+            cmds = ["python", "correction.py", "/opt/data/13.srt", "test"],
             name="task-"+project+"-srt-correction",
             task_id="task-"+project+"-srt-correction",
             in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
@@ -35,7 +43,8 @@ def create_dag(schedule, default_args):
             #resources=compute_resources,
             is_delete_operator_pod=True,
             get_logs=True,
-            secrets = [secret_env]
+            secrets = [secret_env],
+            volume_mounts=volume_mount
         )
         srt_correction
 
