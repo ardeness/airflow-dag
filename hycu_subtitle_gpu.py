2fcf3f2de2518473fc356f6691508ce29a72c47f
Soohyun Park <ardeness@gmail.com>
2024-11-01 09:10:11 +0900

    Upload _sync_post_rag.srt

--- hycu_subtitle_gpu.py
+++ hycu_subtitle_gpu.py
@@ -159,7 +159,7 @@ def create_dag(schedule, default_args):
             image = "024848470331.dkr.ecr.ap-northeast-2.amazonaws.com/hycu/setup:latest",
             image_pull_secrets=[k8s.V1LocalObjectReference("ecr")],
             image_pull_policy='IfNotPresent',
-            cmds = ["python", "cleanup.py", file_prefix+"_sync_post.srt", file_prefix+"_sync_post.score", file_prefix+"_sync_post_rag.score"],
+            cmds = ["python", "cleanup.py", file_prefix+"_sync_post_rag.srt", file_prefix+"_sync_post.srt", file_prefix+"_sync_post.score", file_prefix+"_sync_post_rag.score"],
             name="task-"+project+"-cleanup",
             task_id="task-"+project+"-cleanup",
             in_cluster=in_cluster,  # if set to true, will look in the cluster, if false, looks for file
