46a79921efb491ff999adcff047e0c074f16c6a5
Soohyun Park <ardeness@gmail.com>
2025-03-10 08:18:31 +0900

    Revert memory contraints

--- hycu_subtitle_gpu_no_llm.py
+++ hycu_subtitle_gpu_no_llm.py
@@ -65,13 +65,9 @@ def create_dag(schedule, default_args):
         name="efs-claim",
         mount_path="/workspace/data"
     )
-    #asr_compute_resources = k8s.V1ResourceRequirements(
-    #   requests={"nvidia.com/gpu": "1", "memory": "23Gi"},
-    #   limits={"nvidia.com/gpu": "1", "memory": "23Gi"}
-    #)
     asr_compute_resources = k8s.V1ResourceRequirements(
-       requests={"nvidia.com/gpu": "1"},
-       limits={"nvidia.com/gpu": "1"}
+       requests={"nvidia.com/gpu": "1", "memory": "23Gi"},
+       limits={"nvidia.com/gpu": "1", "memory": "23Gi"}
     )
     gpu_toleration = k8s.V1Toleration(
         key= "nvidia.com/gpu",
